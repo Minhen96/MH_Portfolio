c5eae5bb5f67ef1d8e9226250cbb6f88a9d0fbe8
Minhen96 <evanyap96@gmail.com>
2026-01-31 13:09:28 +0800

    SEO. Enhance footer.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     preact({ compat: true }),
-    tailwind()
+    tailwind(),
+    sitemap(),
   ],
+  site: "https://yapminhen.com",
 });
